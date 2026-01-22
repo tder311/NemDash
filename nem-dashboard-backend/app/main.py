2871bd75694bf38a2337dbcd1f3e193a8b58118f
Tom Derrick <tom.derrick@modoenergy.com>
2026-01-22 16:54:34 +1100

    Fix missing BackgroundTasks import

--- nem-dashboard-backend/app/main.py
+++ nem-dashboard-backend/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from datetime import datetime
